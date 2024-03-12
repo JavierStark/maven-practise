2b8f34dc286f7a4aa0aca0c5299c764957117ba0
JavierStark <javiertorralbocortes@gmail.com>
2024-03-12 09:38:43 +0100

    Constructor to English

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -64,10 +64,10 @@ public class Date {
    *              2050 for validation
    *              to pass).
    */
-  public Date(int tag, int monat, int jahr) {
-    this.day = tag;
-    this.month = monat;
-    this.year = jahr;
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
   }
 
   /**
